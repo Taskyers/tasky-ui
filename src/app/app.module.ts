a0e924a3846a5bd13513127dee321dc3ed5dd739
jsildatk <jakub.sildatk@gmail.com>
2019-10-23 19:28:10 +0000

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-import {Injector} from '@angular/core';
+import { Injector } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './components/registration/registration.component';
@@ -10,9 +10,11 @@ import { HomeComponent } from './components/home/home.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RegistrationMessagesComponent } from './shared/messages/registration-messages/registration-messages.component';
 import { RegistrationValidatorService } from './shared/validators/registration/registration-validator.service';
+import { FooterComponent } from './components/footer/footer.component';
 import { LoginComponent } from './components/login/login.component';
 import { LoginMessagesComponent } from './shared/messages/login-messages/login-messages.component';
 import { SecureComponent } from './components/secure/secure.component';
+import { HeaderHomeComponent } from './components/header-home/header-home.component';
 
 export let InjectorInstance: Injector;
 
@@ -23,32 +25,32 @@ const appRoutes: Routes = [
   { path: '', component: HomeComponent }
 ];
 
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    RegistrationComponent,
-    RegistrationMessagesComponent,
-    HomeComponent,
-    LoginComponent,
-    LoginMessagesComponent,
-    SecureComponent,
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    RouterModule.forRoot(appRoutes),
-    FormsModule,
-    ReactiveFormsModule,
-  ],
-  providers: [
-    RegistrationValidatorService
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        RegistrationComponent,
+        RegistrationMessagesComponent,
+        HomeComponent,
+        FooterComponent,
+        LoginComponent,
+        LoginMessagesComponent,
+        SecureComponent,
+        HeaderHomeComponent,
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        RouterModule.forRoot(appRoutes),
+        FormsModule,
+        ReactiveFormsModule,
+    ],
+    providers: [
+        RegistrationValidatorService
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor(private injector: Injector)
-  {
-    InjectorInstance = this.injector;
-  }
+    constructor(private injector: Injector) {
+        InjectorInstance = this.injector;
+    }
 }
