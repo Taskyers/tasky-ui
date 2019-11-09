d0239d745ff2599e96d2e27fc1e04a4fb2f28f0b
mrzeppa <mrzeppa16@gmail.com>
2019-11-09 18:52:24 +0100

    Create error handler for server statuses

---
+++ src/app/errorHandler.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class ErrorHandler {
+    constructor(
+        private router: Router
+    ) { }
+
+    public handleError(err: any) {
+        if ( err.status === 403 ) {
+            this.router.navigate([ '/pageNotFound' ]);
+        }
+    }
+}
